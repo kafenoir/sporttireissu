d7b0954b732c853ddc0ab8a2490243e9a4acf316
Antti_Ojala <antti.i.ojala@helsinki.fi>
2020-03-19 23:51:02 +0200

    Adding and editing trips

--- application/trips/models.py
+++ application/trips/models.py
@@ -2,10 +2,19 @@ from application import db
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(144), nullable=False)
-    start_date = db.Column(db.DateTime, default=db.func.current_timestamp())
-    end_date = db.Column(db.DateTime, default=db.func.current_timestamp())
-    registration_dl = db.Column(db.DateTime, default=db.func.current_timestamp())
+    name = db.Column(db.String(32), nullable=False)
+    destination = db.Column(db.String(32), nullable=False)
+    start_date = db.Column(db.DateTime, default=db.func.current_date())
+    end_date = db.Column(db.DateTime, default=db.func.current_date())
+    price = db.Column(db.Integer, nullable = False)
+    description = db.Column(db.String(144), nullable=False)
+    registration_dl = db.Column(db.DateTime, default=db.func.current_date())
+    max_participants = db.Column(db.Integer, nullable=False)
 
     def __init__(self, name):
         self.name = name
+        self.destination = "Ei määritelty"
+        self.price = 0
+        self.description = "Tervetuloa matkalleni!"
+        self.max_participants = 10
+        
--- application/trips/views.py
+++ application/trips/views.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.trips.models import Trip
+import datetime
 
 @app.route("/trips/", methods=["GET"])
 def trips_index():
@@ -10,11 +11,42 @@ def trips_index():
 def trips_form():
     return render_template("trips/new.html")
 
+
+@app.route("/trips/<trip_id>", methods=["POST"])
+def trips_editor(trip_id):
+    return render_template("trips/edit.html", trip = Trip.query.get(trip_id))
+
+
 @app.route("/trips/", methods=["POST"])
 def trips_create():
     t = Trip(request.form.get("name"))
+
+    t.price = request.form.get("price")
+    t.destination = request.form.get("destination")
+    t.start_date = datetime.datetime.strptime(request.form.get("start_date"), "%Y-%m-%d").date()
+    t.end_date = datetime.datetime.strptime(request.form.get("end_date"), "%Y-%m-%d").date()
+    t.description = request.form.get("description")
+    t.registration_dl = datetime.datetime.strptime(request.form.get("registration_dl"), "%Y-%m-%d").date()
+    t.max_participants = request.form.get("max_participants")
 
     db.session().add(t)
     db.session().commit()
 
     return redirect(url_for("trips_index"))
+
+@app.route("/trips/<trip_id>/", methods=["POST"])
+def trips_edit(trip_id):
+
+    t = Trip.query.get(trip_id)
+    t.name = request.form.get("name")
+    t.price = request.form.get("price")
+    t.destination = request.form.get("destination")
+    t.start_date = datetime.datetime.strptime(request.form.get("start_date"), "%Y-%m-%d").date()
+    t.end_date = datetime.datetime.strptime(request.form.get("end_date"), "%Y-%m-%d").date()
+    t.description = request.form.get("description")
+    t.registration_dl = datetime.datetime.strptime(request.form.get("registration_dl"), "%Y-%m-%d").date()
+    t.max_participants = request.form.get("max_participants")
+    db.session().commit()
+
+    return redirect(url_for("trips_index"))
+
